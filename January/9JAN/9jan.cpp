2ef2b849debff8c7793cf4219af9d1a38eb04d95
ashcodeshere <ashcodeshere@gmail.com>
2026-01-09 08:05:18 +0530

    day 9(had to leave early)

---
+++ January/9JAN/9jan.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <vector>
+#include <unordered_map>
+using namespace std;
+int main(){
+    // vector<int>arr;
+    // int n;
+    // cout<<"Enter no. of element";
+    // cin>>n;
+    // for(int i=0;i<n;i++) cin>>n;
+    // unordered_map<int,int>ump;
+	// for(int i=0;i<n;i++){
+    //     ump[arr[i]]++;
+    //     if(ump[arr[i]]>1) return arr[i];
+    // }
+    // return -1;
+}
